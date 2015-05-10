11380b9a1dc3a8fe39226608f7b7fae6e9afcf3e
Spaztazcular <blooddragon3190@yahoo.com>
2015-05-09 23:29:03 -0500

    minimap and itemBar
    itemBar I think needs some work, but should work.

---
+++ Scripts/itemBar.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class itemBar : MonoBehaviour {
+
+    public Image filling;
+
+    float amount;
+    float limit;
+
+    //bool fillingCurrent;
+
+	// Use this for initialization
+	void Start () {
+
+        //amount = .52f;
+        //limit = 0.0f;
+
+        //fillingCurrent = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        /*if (fillingCurrent == true)
+        {
+            Debug.Log("hit filling");
+
+            filling.fillAmount += 0.01f;
+            limit += 0.01f;
+
+            if (limit >= amount)
+                fillingCurrent = false;
+        }*/
+	
+	}
+
+    //should be percentage
+    void fillAmount(float itemAmount)
+    {
+        filling.fillAmount += itemAmount;
+        //fillingCurrent = true;
+    }
+}
---
+++ Scripts/minimap.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class minimap : MonoBehaviour {
+
+    public Image baseImage;
+    public Image[] location;
+
+    float timer;
+
+    private int roomLocation;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0.0f;
+
+        for(int i = 1; i < 17; i++)
+        {
+            location[i].enabled = false;
+        }
+
+        roomLocation = 0;
+        location[roomLocation].enabled = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+
+        if (timer >= 1.0f)
+        {
+            location[roomLocation].enabled = false;
+            roomLocation++;
+            if (roomLocation > 16)
+            {
+                roomLocation = 0;
+            }
+            location[roomLocation].enabled = true;
+            timer = 0.0f;
+        }
+	
+	}
+
+    //broadcast method
+    void locationSet(int currentRoom)
+    {
+        location[roomLocation].enabled = false;
+        roomLocation = currentRoom;
+        location[roomLocation].enabled = true;
+    }
+}
